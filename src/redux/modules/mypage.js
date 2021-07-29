92e431d72de2e737bb4e73ff8dbcb90aa2c07ab8
qlemql <taehyun_fe@naver.com>
2021-07-30 01:02:53 +0900

    [docs] mypage redux 뼈대 작성

--- src/redux/modules/mypage.js
+++ src/redux/modules/mypage.js
@@ -1,2 +1,26 @@
 import { createAction, handleActions } from "redux-actions";
 import produce from "immer";
+import { MypageApis } from "../../shared/api";
+
+const GET_MYCHALLENGE = "GET_MYCHALLENGE";
+const EDIT_MYPROFILE = "EDIT_MYPROFILE";
+
+const getMyChallenge = createAction(GET_MYCHALLENGE, () => ({}));
+const editMyProfile = createAction(EDIT_MYPROFILE, () => ({}));
+
+const initialState = {};
+
+export default handleActions(
+  {
+    [GET_MYCHALLENGE]: (state, action) => produce(state, (draft) => {}),
+    [EDIT_MYPROFILE]: (state, action) => produce(state, (draft) => {}),
+  },
+  initialState
+);
+
+const actionCreators = {
+  getMyChallenge,
+  editMyProfile,
+};
+
+export { actionCreators };
