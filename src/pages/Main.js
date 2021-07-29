09c37efe37e70dae534c0fb9acbb853c71b0803a
qlemql <taehyun_fe@naver.com>
2021-07-30 02:30:33 +0900

    [feat] 로그인시 유저정보 가져오기 & 비로그인시 정보가져오기 기능

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,47 +1,46 @@
-import React, {useEffect} from "react";
-import styled from 'styled-components';
+import React, { useEffect } from "react";
+import styled from "styled-components";
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import MainSlider from '../components/MainSlider';
-import Category from '../components/Category';
-import { MainCreators } from '../redux/modules/main';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import MainSlider from "../components/MainSlider";
+import Category from "../components/Category";
+import { MainCreators } from "../redux/modules/main";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { getCookie } from "../shared/Cookie";
 
 const Main = (props) => {
-    const islogin = useSelector((state) => state.user.isLogin);
-    const dispatch = useDispatch();
-    
-    useEffect(() => {
-        if (islogin) {
-            dispatch(MainCreators.userLoadDB());
-            dispatch(MainCreators.userLoad(null));
-        }else{
-            dispatch(MainCreators.guestLoadDB());
-        }
-    },[])
+  const dispatch = useDispatch();
 
-    return(
-        <React.Fragment>
-            <Header/>
-                <Container>
-                    <MainSlider/>
-                    <Category/>
-                </Container>
-            <Footer/>
-        </React.Fragment>
-        )
+  useEffect(() => {
+    if (getCookie("token")) {
+      dispatch(MainCreators.userLoadDB());
+    } else {
+      dispatch(MainCreators.guestLoadDB());
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Header />
+      <Container>
+        <MainSlider />
+        <Category />
+      </Container>
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default Main;
 
 const Container = styled.div`
-    max-width: 43.75em;
-    margin: 0 auto;
-    width: 100%;
-    position: absolute;
-    left: 50%;
-    top: 6em;
-    transform: translateX(-50%);
+  max-width: 43.75em;
+  margin: 0 auto;
+  width: 100%;
+  position: absolute;
+  left: 50%;
+  top: 6em;
+  transform: translateX(-50%);
 `;
--- src/redux/modules/main.js
+++ src/redux/modules/main.js
@@ -1,63 +1,70 @@
-import { createAction, handleActions } from 'redux-actions';
-import produce from 'immer';
-import { MainApis } from '../../shared/api';
+import { createAction, handleActions } from "redux-actions";
+import produce from "immer";
+import { MainApis } from "../../shared/api";
 
 // action
-const G_LOAD = 'main/G_LOAD';
-const M_LOAD = 'main/M_LOAD';
+const G_LOAD = "main/G_LOAD";
+const M_LOAD = "main/M_LOAD";
 
 // action creator
-const guestLoad = createAction(G_LOAD, (guestmain) => ({guestmain}));
-const userLoad = createAction(M_LOAD, (usermain) => ({usermain}));
+const guestLoad = createAction(G_LOAD, (guestmain) => ({ guestmain }));
+const userLoad = createAction(M_LOAD, (usermain) => ({ usermain }));
 
 // initialState
 
 const initialState = {
-    guestmain:[],
-    usermain:[],   
-}
+  guestmain: [],
+  usermain: [],
+};
 
 // Thunk function
 const guestLoadDB = () => {
-    return function(dispatch, getState, {history}){
-        MainApis
-        .guestMain()
-        .then((res) => {
-            dispatch(guestLoad(res.data))
-        }).catch((err) => {
-            console.log(err)
-        })
-    }
-}
+  return function (dispatch, getState, { history }) {
+    MainApis.guestMain()
+      .then((res) => {
+        dispatch(guestLoad(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+};
 
 const userLoadDB = () => {
-    return function(dispatch, getState, {history}){
-        MainApis
-        .userMain()
-        .then((res) => {
-            dispatch(userLoad(res.data))
-        }).catch((err) => {
-            console.log(err)
-        })
-    }
-}
+  return function (dispatch, getState, { history }) {
+    MainApis.userMain()
+      .then((res) => {
+        dispatch(userLoad(res.data));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+};
 
 // reducer
 
-export default handleActions({
-    [G_LOAD]:(state,action) => produce(state, (draft) => {
+export default handleActions(
+  {
+    [G_LOAD]: (state, action) =>
+      produce(state, (draft) => {
         draft.guestmain = action.payload.guestmain;
-    }),
-    [M_LOAD]:(state,action) => produce(state,(draft) => {
+        draft.usermain = [];
+      }),
+    [M_LOAD]: (state, action) =>
+      produce(state, (draft) => {
         draft.usermain = action.payload.usermain;
-    })
-}, initialState)
+        draft.guestmain = [];
+      }),
+  },
+  initialState
+);
 
 const MainCreators = {
-    guestLoadDB,
-    userLoadDB,
-    userLoad,
-    guestLoad,
-}
+  guestLoadDB,
+  userLoadDB,
+  userLoad,
+  guestLoad,
+};
 
-export { MainCreators }
+export { MainCreators };
