03edf51ebf85777aacc200a901ae180f36c16e1f
minjujung <love053011@gmail.com>
2021-08-03 01:07:37 +0900

    Edit Point and Level logic

--- src/components/MyPieces.js
+++ src/components/MyPieces.js
@@ -4,32 +4,43 @@ import levelData from "../level";
 import { useSelector } from "react-redux";
 
 function MyPieces(props) {
+  const { memberLevel, point, pointHistoryList } = props;
   const user_info = useSelector((state) => state.user.userInfo);
 
-  const [levelState, setLevelState] = useState();
+  const userLevel = user_info.memberLevel;
+  const levelDivided5 = parseInt((userLevel - 1) / 5) + 1;
 
-  // const userLevel = user_info.memberLevel;
-  // const levelDivided5 = parseInt(userLevel - 1 / 5) + 1;
+  let levelState = 0;
+  if (userLevel <= 5) {
+    levelState = (((userLevel - 1) % 5) + 1) * (levelDivided5 * 100);
+  } else if (5 < userLevel && userLevel <= 10) {
+    levelState = (((userLevel - 1) % 5) + 1) * (levelDivided5 * 100) + 500;
+  } else if (10 < userLevel && userLevel <= 15) {
+    levelState = (((userLevel - 1) % 5) + 1) * (levelDivided5 * 100) + 1500;
+  } else if (15 < userLevel && userLevel <= 20) {
+    levelState = (((userLevel - 1) % 5) + 1) * (levelDivided5 * 100) + 3000;
+  } else if (20 < userLevel && userLevel <= 25) {
+    levelState = (((userLevel - 1) % 5) + 1) * (levelDivided5 * 100) + 5000;
+  } else {
+    levelState = (((userLevel - 1) % 5) + 1) * (levelDivided5 * 100) + 7500;
+  }
 
-  // if (userLevel <= 5) {
-  //   setLevelState((((userLevel - 1) % 5) + 1) * (levelDivided5 * 100));
-  // }
+  const levelDataIdx = parseInt(user_info.memberLevel - 1) / 5;
 
-  // console.log(levelState);
+  const levelArray = Array.from({ length: levelDivided5 }, (item, idx) => {
+    return idx;
+  });
 
-  // const levelArray = Array.from({ length: levelDivided5 }, (item, idx) => {
-  //   return idx;
-  // });
   return (
     <>
       <section>
         <div>
-          <h3>나의 등급:</h3>
-          {/* <img
-            src={levelData[parseInt(user_info.memberLevel - 1 / 5)]?.img}
+          <h3>나의 등급: {userLevel}</h3>
+          <img
+            src={levelData[levelDataIdx] && levelData[levelDataIdx].img}
             alt="level_image"
             style={{ width: "5em", height: "5em" }}
-          />{" "} */}
+          />{" "}
           level {user_info.memberLevel}
         </div>
         <div>
@@ -38,19 +49,18 @@ function MyPieces(props) {
         </div>
         <div>
           <h3>모은 구슬: </h3>
-          {/* {levelArray.map((idx) => (
+          {levelArray.map((idx) => (
             <img
               src={levelData[idx].img}
               alt="level_img"
               style={{ width: "5em", height: "5em" }}
             />
-          ))} */}
+          ))}
         </div>
       </section>
       <section>
         <h4>조각 모음 일지</h4>
-
-        {user_info.pointHistoryList?.map((history) => (
+        {pointHistoryList.map((history) => (
           <div key={history.pointHistoryId}>
             <p>
               <strong>{history.challengeTitle}</strong> 챌린지에서 조각을
