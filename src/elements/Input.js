d919733ae1cff8a23a4737a3ef2ec3e5d910297a
WonJunPyun <84018317+WonJunPyun@users.noreply.github.com>
2021-08-05 05:08:12 +0900

    [뷰] 로그인 회원가입 헤더 완성
    [뷰] 로그인 회원가입 헤더 완성

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -1,51 +1,40 @@
 import React from "react";
 import styled from "styled-components";
 
-function Input(props) {
-  const {
+const Input = ({ 
+  width,
+  type, 
+  placeholder,
+  id,
+  }) => {
+  const styles =
+  { 
     width,
-    label,
-    placeholder,
     type,
-    multiLine,
-    value,
-    is_Submit,
-    onSubmit,
-    onChange,
-  } = props;
-
-  const styles = { width: width };
+    placeholder,
+    id,
+  };
   return (
-    <>
-      <ElInput
-        {...styles}
-        label={label}
-        placeholder={placeholder}
-        type={type}
-        onChange={onChange}
-      />
-    </>
+    <ElInput 
+    {...styles} 
+    placeholder={placeholder} 
+    type={type} 
+    id={id}
+    /> 
   );
-}
+};
 
 Input.defaultProps = {
   width: "100%",
-  multiLine: false,
-  label: false,
-  placeholder: "텍스트를 입력해주세요.",
-  type: "text",
-  is_Submit: false,
-  onChange: () => {},
+  placeholder: "",
+  type: "",
+  id: "",
+  bg : "#ffffff",
 };
 
 const ElInput = styled.input`
-  border-bottom: 1px solid ${({ theme }) => theme.colors.gray};
   width: ${(props) => props.width};
-  padding: 12px 4px;
-  box-sizing: border-box;
-  :focus {
-    border: 2px solid ${({ theme }) => theme.colors.mainGreen};
-  }
+  border-bottom: 2px solid ${({theme}) => theme.colors.black}
 `;
 
 export default Input;
